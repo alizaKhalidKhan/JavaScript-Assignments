c86bf9f97e90db83fe48a9ccc6fbb343a52b6de1
Aliza Khalid <alizajuw@gmail.com>
2024-10-10 20:10:04 +0500

    Create file.js

---
+++ JS Chapter 1/file.js
@@ -0,0 +1,18 @@
+--What is the keyword that displays a message box? 
+Ans : alert
+
+--Type the 2 characters that are missing in this statement.
+alert"You're learning JavaScript!";
+Ans : alert("You're learning JavaScript!");
+
+--A __________ ends in a semicolon.
+Ans : Statement
+
+--Code an alert statement displaying any message you like.
+Ans : alert("Hello world!");
+
+--What are the 5 punctuation characters of an alert statement, in order?
+Ans : ("");
+
+-- Code an alert statement that displays your full name.
+Ans : alert("Aliza Khalid Hussain Khan!!");
